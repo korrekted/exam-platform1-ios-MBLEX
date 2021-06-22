da0013dbbd7b903b78be706fceb93d0dc73f779a
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-22 19:30:48 +0300

    build 1

---
+++ MBLEX/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 247, green: 252, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 66, green: 68, blue: 229)
+    static let mainColorAlpha = UIColor(integralRed: 66, green: 68, blue: 229, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 255, green: 147, blue: 143)
+    
+    static let progress1Color = UIColor(integralRed: 66, green: 68, blue: 229)
+    static let progress2Color = UIColor(integralRed: 255, green: 147, blue: 143)
+    static let progress3Color = UIColor(integralRed: 148, green: 52, blue: 237)
+}
